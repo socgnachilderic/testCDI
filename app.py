a3145ef932fb23cd26f2bbca0304f2e893b0b32b
SOCGNA KOUYEM Childéric <westbook90@gmail.com>
2020-05-28 09:23:20 +0100

    Initialisation du repository

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+def generate_list():
+    return [f"liste {i}" for i in range(1, 5)]
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", lists=generate_list())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
